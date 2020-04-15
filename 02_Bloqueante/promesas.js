f5e94d373dc2e2ecdd7fc75cded8101ef30a1ba9
alce65 <alce65@hotmail.es>
2020-04-15 18:08:32 +0200

    Promesas

---
+++ 02_Bloqueante/promesas.js
@@ -0,0 +1,35 @@
+function hacerAlgo() {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            let n = Math.random()
+            if (n < 0.5) {
+                resolve(n)
+            } else {
+                reject(new Error(n))
+            }
+        }, 1000)
+    })
+}
+
+// En ES6 - ES2015
+
+const promesa = hacerAlgo()
+console.log(promesa)
+promesa
+.then((resp) => {
+    console.log('Ok', resp)
+})
+.catch( (err) => {
+    console.log('Error', err.message )
+});
+
+//ES 2017  async / await
+
+( async () => {
+    try {
+        const resp = await hacerAlgo()
+        console.log('Ok desde await', resp)        
+    } catch (err) {
+        console.log('Error desde await', err.message )
+    }
+})()
